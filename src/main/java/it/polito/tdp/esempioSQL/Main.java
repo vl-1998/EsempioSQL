d1a38093f1de128412bfaba741d7259733353a8d
Fulvio Corno <fulvio.corno@polito.it>
2020-03-25 16:30:14 +0100

    Primo esempio JDBC

---
+++ src/main/java/it/polito/tdp/esempioSQL/Main.java
@@ -0,0 +1,7 @@
+package it.polito.tdp.esempioSQL;
+
+public class Main {
+	public static void main(String[] args) {
+        EntryPoint.main(args);
+    }
+}
---
+++ src/main/java/it/polito/tdp/esempioSQL/model/LeggiBabs.java
@@ -0,0 +1,48 @@
+package it.polito.tdp.esempioSQL.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class LeggiBabs {
+	
+	public void run() {
+		String jdbcURL = "jdbc:mysql://localhost/babs?user=root&password=root" ;
+		
+		try {
+			Connection conn = DriverManager.getConnection(jdbcURL) ;
+			
+			Statement st = conn.createStatement() ;
+			
+			String sql = "SELECT NAME FROM station" ;
+			
+			ResultSet res = st.executeQuery(sql) ;
+			
+			while(res.next()) {
+				String nomeStazione = res.getString("name") ;
+				
+				System.out.println(nomeStazione) ;
+			}
+			st.close();
+						
+			conn.close();
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		// FACTORY: creazione di un oggetto di una classe, senza
+		// conoscere il tipo della classe (NON potevo usare new)
+		// Uso un metodo fornito da un'altra classe che internamente
+		// farà new e conoscerà il tipo di classe effettivo.
+	}
+	
+	public static void main(String args[]) {
+		LeggiBabs babs = new LeggiBabs() ;
+		babs.run();
+	}
+
+}
